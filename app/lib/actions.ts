9f57e1c9d7dc7eb5e9a3dcc05577f58e36c58c17
Josh O'Bryan <vespergo@gmail.com>
2024-01-01 21:44:02 -0600

    Adding authentication and authorization.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -27,6 +29,25 @@ const FormSchema = z.object({
     message?: string | null;
   };
 
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
+  
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
